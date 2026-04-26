bc81888d30a677e6e6af7e98284940596ae7307f
shivam-raghav8439 <shivamraghavpalra@gamil.com>
2026-04-27 01:07:52 +0530

    Add a new foler

---
+++ python course/chapter8_function and Recursion/function.py
@@ -0,0 +1,35 @@
+# a = int(input("Enter number :"))
+# b =int(input("Enter number :"))
+# c= int(input("Enter number :"))
+# print("the average of three number is ",(a+b+c)/3)
+
+# x= int(input("Enter number :"))
+# y=int(input("Enter number :"))
+# z=int(input("Enter number :"))
+# average = (x+y+z)/3
+# print(average)
+
+# def average():
+#     x= int(input("Enter number :"))
+#     y=int(input("Enter number :"))
+#     z=int(input("Enter number :"))
+#     average = (x+y+z)/3
+#     print(average)
+
+# average()
+# average()
+# average()
+# average()
+# average()
+
+def fun():
+    print("Hello Raghav ! ")
+fun()
+print("Thank You !")
+fun()
+print("Thank You !")
+fun()                      # function call  
+print("Thank You !")
+fun()
+print("Thank You !")
+fun() 
---
+++ python course/chapter8_function and Recursion/function1.py
@@ -0,0 +1,29 @@
+# def inv(name , ending):
+#     print("Have a nice day, " +  name + ending)
+#     print(ending)
+# inv("Raghav", "Thankyou")
+# inv("Shiavm", "Thankyou")
+# inv("Suriya", "Thankyou")
+# inv("Shivani", "Thanks")
+
+# def sorry(name):
+#     print("Sorry!" + name)
+# sorry("Raghav")
+# sorry("Suriya")
+# sorry("Thakur")
+# sorry("Rajput")
+# sorry("Shivani")
+
+def greet(name,ending):
+    print("My Self Raghav ! " + name)
+    print(ending)
+    return "Done"
+
+a= greet("Welcome Raghav!", "Jai Shree Ram")
+greet("Welcome Suriya!", "Jai Shree Ram")
+print(a)
+
+def ram(name,ending):
+    print(f"Hello {name}")
+ram("Raghav") 
+    
